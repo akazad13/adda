856a6ab6377269806d854df815295779e26d8f43
Md. Abul Kalam <akazad.cse13@gmail.com>
2020-01-20 21:52:16 +0600

    Added home, nav and registration component

---
+++ DatingApp-Front/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+  @Output() cancelRegister =  new EventEmitter();
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration successful');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
